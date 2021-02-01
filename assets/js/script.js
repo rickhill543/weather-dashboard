bcb3ac5adbaf19905ced2f6f4bb0bb9fa6d5e721
jWalker <rickhill543@gmail.com>
2021-01-31 19:16:29 -0500

    setup dynamic data display & history panel

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,7 @@
 // global variables
 var currentDate = moment().format('l');
 var weatherForm = document.querySelector('#weather-form');
+var weatherButton = document.querySelector('#weather-button');
 var cityInput = document.querySelector('#city-input');
 var historyContainer = document.querySelector('#history-container');
 var searchedCity = document.querySelector('#searched-city');
@@ -36,6 +37,12 @@ function weatherSearchCurrent() {
         searchLon = response.coord.lon;
         currentIcon = response.weather[0].icon;
         currentIconUrl = 'http://openweathermap.org/img/w/' + currentIcon + '.png';
+        // applies the dynamic api date to the correct element locations
+        searchedCity.textContent = response.name + ' (' + currentDate + ') ';
+        searchedIcon.setAttribute('src', currentIconUrl);
+        searchedTemp.textContent = 'Temperature: ' + response.main.temp + ' \u00B0F';
+        searchedHumidity.textContent = 'Humidity: ' + response.main.humidity + '%';
+        searchedWind.textContent = 'Wind Speed: ' + response.wind.speed + ' MPH';
         console.log(response);
         weatherSearchUv();
       });
@@ -55,6 +62,17 @@ function weatherSearchCurrent() {
       })
       .then(function(response) {
         uvIndex = response.value
+        searchedUvText.textContent = 'UV Index: ';
+        searchedUv.textContent = uvIndex;
+        searchedUv.classList.remove('uv-favorable', 'uv-moderate', 'uv-severe')
+        // conditional statement to dynamically apply css classes based on uv index severity
+        if (response.value < 4) {
+          searchedUv.classList.add('uv-favorable')
+        } else if (response.value < 7) {
+          searchedUv.classList.add('uv-moderate')
+        } else {
+          searchedUv.classList.add('uv-severe')
+        }
         console.log(response);
         weatherSearchFuture();
       });
@@ -82,6 +100,19 @@ function weatherSearchCurrent() {
           var day = date.getDate()
           var year = date.getFullYear();
           var displayDate = month + '/' + day + '/' + year;
+          console.log("date: " + date);
+          loopDay.textContent = displayDate;
+          // uses a loop to dynamically generate a 5 card panel with relevant api information
+          var loopIcon = document.querySelector('#future-icon-' + i);
+          var iconSub = response.list[n].weather[0].icon;
+          iconSubUrl = 'http://openweathermap.org/img/w/' + iconSub + '.png';
+          loopIcon.setAttribute('src', iconSubUrl);
+
+          var loopTemp = document.querySelector('#future-temp-' + i);
+          loopTemp.textContent = 'Temp: ' + response.list[n].main.temp + ' \u00B0F';
+
+          var loopHumidity = document.querySelector('#future-humidity-' + i);
+          loopHumidity.textContent = 'Humidity: ' + response.list[n].main.humidity + '%';
         }
         console.log(response);
       });
@@ -95,5 +126,26 @@ function weatherSearchCurrent() {
     if (cityInput.value.length < 1 || cityInput.value.length > 20) {
       return;
     }
+    console.log("right before for loop checking cityInpyt: " + cityInput.value);
+      for (i=0; i<searchHistory.length; i++) {
+        if (cityInput.value == searchHistory[i].value) {
+          console.log("said true before return");
+          return;
+        }
+    }
+    $("#history-container").append('<li>' + cityInput.value + '</li>');
+      // next few statements collect and store relevant information for later use
+      lastSearched = cityInput.value;
+      searchHistory.push([lastSearched]);
       weatherSearchCurrent();
+  });
+
+  // History Container Event Listener for loading up old searches
+  historyContainer.addEventListener('click', function(event) {
+    event.preventDefault();
+    // uses event delegation to select the correct element
+    var archivedSearch = event.target.textContent;
+    cityInput.value = archivedSearch;
+    lastSearched = archivedSearch;
+    weatherSearchCurrent();
   });
